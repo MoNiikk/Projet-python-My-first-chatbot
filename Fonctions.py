05edf0590095f0c2016380575d7aa3ab015fcea3
Rafael Fellbom <rafael.fellbom@gmail.com>
2023-12-09 14:55:57 +0100

    .git/COMMIT_EDITMSG["main maj"] [mainmaj] i :wq

--- Fonctions.py
+++ Fonctions.py
@@ -7,9 +7,6 @@ def list_of_files(directory, extension):
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
-directory = "C:/Users/rafaf/OneDrive/pythonProject2/speeches"
-files_names = list_of_files(directory, ".txt")
-print(files_names)
 
 
 def pres_names(files_names):
@@ -28,8 +25,7 @@ def pres_names(files_names):
     return(list_pres_names)
 
 
-list = pres_names(files_names)
-print(list)
+
 
 
 def prenom_pres(list) :
@@ -39,8 +35,7 @@ def prenom_pres(list) :
     return(liste_prenoms)
 
 
-liste_prenoms = prenom_pres(list)
-print(liste_prenoms)
+
 
 
 def minuscule (directory, directory2):
@@ -50,20 +45,20 @@ def minuscule (directory, directory2):
                 if ord(lettre)>64 and ord(lettre)<91:
                     lettre=chr(ord(lettre)+32)
                 f2.write(lettre)
-directory2 = "C:/Users/rafaf/OneDrive/pythonProject2/cleaned"
 
-minuscule(directory, directory2)
+
+
 def suppr_ponct(directory, directory2) :
     liste_ponct = [',', '.', ';', '-', ':', '!', '_', '?', "'", '(', ')', '[', ']']
     for name in os.listdir(directory2) :
-        with open(directory2 + '/' + name, 'r') as f :
-            with open(directory + '/' + name + '3', 'x') as f2 :
+        with open(directory2 + '/' + name, 'r') as f:
+            with open(directory2 + '/' + name + '3', 'x') as f2:
                 for lettre in f.read() :
                     if lettre not in liste_ponct :
                         f2.write(lettre)
                     elif lettre == "'" or lettre == '-' :
-                        f2.write(' ')
+                        f2.write(" ")
                     else :
-                        f2.write('')
+                        f2.write("")
 
-suppr_ponct(directory, directory2)
+
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import os
+import Fonctions as fct
+
+directory2 = "C:/Users/rafaf/OneDrive/pythonProject2/cleaned"
+directory = "C:/Users/rafaf/OneDrive/pythonProject2/speeches"
+files_names = fct.list_of_files(directory, ".txt")
+print(files_names)
+
+list = fct.pres_names(files_names)
+print(list)
+
+liste_prenoms = fct.prenom_pres(list)
+print(liste_prenoms)
+
+fct.minuscule(directory, directory2)
 
 
+fct.suppr_ponct(directory, directory2)
