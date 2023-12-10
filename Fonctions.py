abbe7f75ac9b213e0082f8de7be2044036d8ec23
Rafael Fellbom <rafael.fellbom@gmail.com>
2023-12-10 16:07:11 +0100

    Fonction Tf ajoutée

--- Fonctions.py
+++ Fonctions.py
@@ -1,62 +1,74 @@
 import os
 
-#Fonction
+
+#Fonction d'extraction du nom des fichiers
 def list_of_files(directory, extension):
-    files_names =[]
+    files_names = []
     for filename in os.listdir(directory):
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
 
 
+#Fonction pour lister les noms des présidents
 def pres_names(files_names):
     list_pres_names1 = []
-    for name in files_names :
+    for name in files_names:
         if 'Chirac2' in name or 'Mitterrand2' in name:
-            del(name)
-        else :
+            del name
+        else:
             list_pres_names1.append(name.split('_')[1])
     list_pres_names = []
-    for name in list_pres_names1 :
+    for name in list_pres_names1:
         if 'Chirac' in name or 'Mitterrand' in name:
             list_pres_names.append(name.split("1")[0])
-        else :
+        else:
             list_pres_names.append(name.split('.')[0])
-    return(list_pres_names)
+    return list_pres_names
 
+#Fonction pour associer les prénoms aux noms
+def prenom_pres(liste):
+    liste_prenoms = ["Jacques", "Valérie", "François", "Emanuel", "François", "Nicolas"]
+    for i in range(len(liste)):
+        liste_prenoms[i] = liste_prenoms[i] + ' ' + liste[i]
+    return liste_prenoms
 
-
-def prenom_pres(list) :
-    liste_prenoms = ["Jacques", "Valérie", "François", "Manu", "François", "Nico"]
-    for i in range (len(list)) :
-        liste_prenoms[i] = liste_prenoms[i] + ' ' + list[i]
-    return(liste_prenoms)
-
-
-
-
-
-def minuscule (directory, directory2):
+#Fonction pour mettre en minuscules les documents
+def minuscule(directory, directory2):
     for filename in os.listdir(directory):
-        with open(directory +'/' + filename, "r") as f1, open(directory2 + '/' + filename + '2', "x") as f2:
+        with open(directory + '/' + filename, "r") as f1, open(directory2 + '/' + filename + '2', "w") as f2:
             for lettre in f1.read():
-                if ord(lettre)>64 and ord(lettre)<91:
-                    lettre=chr(ord(lettre)+32)
+                if ord(lettre) > 64 and ord(lettre) < 91:
+                    lettre = chr(ord(lettre)+32)
                 f2.write(lettre)
 
-
-
-def suppr_ponct(directory, directory2) :
-    liste_ponct = [',', '.', ';', '-', ':', '!', '_', '?', "'", '(', ')', '[', ']']
-    for name in os.listdir(directory2) :
+#Fonction pour enlever les signes de ponctuations
+def suppr_ponct(directory2):
+    liste_ponct = [',', '.', ';', '-', ':', '!', '', '?', "'", '(', ')', '[', ']']
+    for name in os.listdir(directory2):
         with open(directory2 + '/' + name, 'r') as f:
-            with open(directory2 + '/' + name + '3', 'x') as f2:
-                for lettre in f.read() :
-                    if lettre not in liste_ponct :
-                        f2.write(lettre)
-                    elif lettre == "'" or lettre == '-' :
-                        f2.write(" ")
-                    else :
-                        f2.write("")
+            file = f.read()
+        with open(directory2 + '/' + name.split(".")[0] + ".txt", 'w') as f2:
+            for lettre in file:
+                if lettre not in liste_ponct:
+                    f2.write(lettre)
+                elif lettre == "'" or lettre == '-':
+                    f2.write(" ")
+            f2.seek(0, 2)
+
+
+def TF(directory2):
+    for name in os.listdir(directory2):
+        tf = {}
+        with open(directory2 + '/' + name, 'r') as f:
+            for ligne in f :
+                for mot in ligne.split():
+                    cpt = 0
+                    for mot2 in ligne.split():
+                        if mot in tf :
+                            tf[mot] += 1
+                        else :
+                            tf[mot] = 1
+        print(tf)
 
 
--- main.py
+++ main.py
@@ -6,13 +6,14 @@ directory = "./speeches"
 files_names = fct.list_of_files(directory, ".txt")
 print(files_names)
 
-list = fct.pres_names(files_names)
-print(list)
+liste = fct.pres_names(files_names)
+print(liste)
 
-liste_prenoms = fct.prenom_pres(list)
+liste_prenoms = fct.prenom_pres(liste)
 print(liste_prenoms)
 
 fct.minuscule(directory, directory2)
 
+fct.suppr_ponct(directory2)
 
-fct.suppr_ponct(directory, directory2)
+fct.TF(directory2)
